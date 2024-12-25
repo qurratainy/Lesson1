443d2c15ad6aee79830fb159bc887531c23316f1
qurratainy <qurratainy@gmail.com>
2024-12-25 18:52:38 +0500

    activity2

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ print("Hello Codingal!")
 print("I am QurratulAin")
 print("I am a Software Engineer")
 print("Learning Python with Mr. Hemant")
+
+print("Now I am making changes to my published file")
